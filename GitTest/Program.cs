d34c9b5236594097980342022a2d0e32bc308e1d
alnavski98 <59961638+alnavski98@users.noreply.github.com>
2020-02-01 17:51:31 +0100

    Update Program.cs

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -7,7 +7,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Something");
-            int a = 5;
+            int a = 6;
         }
     }
 }
